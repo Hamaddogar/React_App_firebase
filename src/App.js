6ef00887b0195275b1e096ae0a6ddd02214afe21
Hamaddogar <hamadali77115033@gmail.com>
2020-07-28 10:52:55 +0500

    ReactApp with firebase

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Checkoutmain from '../src/component/checkout-main/checkout-main'
+
+function App() {
+  return (
+   <>
+<Checkoutmain/>
+
+   </>
+  );
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,12 @@
+ import *as  firebase from 'firebase'
+ var firebaseConfig = {
+    apiKey: "AIzaSyCgHjqWEgQgbNTKzq32fEA66RNAEHGnZAo",
+    authDomain: "reactfbproject-b4fc7.firebaseapp.com",
+    databaseURL: "https://reactfbproject-b4fc7.firebaseio.com",
+    projectId: "reactfbproject-b4fc7",
+    storageBucket: "reactfbproject-b4fc7.appspot.com",
+    messagingSenderId: "264875811257",
+    appId: "1:264875811257:web:6003b9177e039ee7fe9668"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
